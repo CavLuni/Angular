f143df00dfd45ca5b09f03b4bc54098849084e43
Denis Luniere <denis.luniere@gmail.com>
2021-05-05 19:51:18 -0300

    Dividindo a aplicação em módulos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -3,8 +3,7 @@ import { CourseService } from './cource.service';
 import { Course } from './course';
 
 @Component({
-    selector: 'app-course-list',
-    templateUrl: './course-list.component.html'
+   templateUrl: './course-list.component.html'
 })
 
 export class CourseListComponent implements OnInit {
@@ -22,8 +21,27 @@ export class CourseListComponent implements OnInit {
  /* ngOnInit = Event Start*/
 
     ngOnInit(): void {
-       this._courses = this.courseService.retrieveAll();
-       this.filteredCourses = this._courses;
+      this.retrieveAll();
+    }
+    retrieveAll(): void {
+       this.courseService.retrieveAll().subscribe({
+            next: courses => {
+                this._courses = courses;
+                this.filteredCourses = this._courses;
+            },
+            error: err => console.log('Error', err)
+       })
+       
+    }
+
+    deleteById(courseId: number): void {
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => {
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
     }
 
     set filter(value: string) {
